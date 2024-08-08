1d9bc85b12930253dbd8b6f09c3c50dfbb6cd4b8
Troy Rice <troyanthonyrice@gmail.com>
2024-08-08 12:24:17 -0700

    spotify login and redirect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import './Home.css'
 
 export default function Home() {
+    const handleLogin = () => {
+        const redirectUri = 'http://127.0.0.1:5173/callback';
+        const clientId = '8edc5a3ad52e4f1f9447e2f91fd06fd3';
+        const scopes = 'user-read-private user-read-email user-read-recently-played';
+
+        window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${redirectUri}&scope=${scopes}`
+    }
+
     return (
-        <h1>Home</h1>
+        <div>
+            <h1>Home</h1>
+            <button onClick={handleLogin}>Login to Spotify</button>
+        </div>
     )
 }
